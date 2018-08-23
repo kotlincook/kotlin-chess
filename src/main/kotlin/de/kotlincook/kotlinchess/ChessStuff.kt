39f0d534b3bc7674b622dc9fd1072b023dfab7ab
javacook <javacook@gmx.de>
2018-08-23 08:08:33 +0200

    ChessStuff finished

--- src/main/kotlin/de/kotlincook/kotlinchess/ChessStuff.kt
+++ src/main/kotlin/de/kotlincook/kotlinchess/ChessStuff.kt
@@ -9,8 +9,32 @@ enum class Color {
 
 enum class HorCoord {
     A, B, C, D, E, F, G, H;
+    companion object {
+        fun iterator() = values().asIterable()
+    }
 }
 
 enum class VertCoord {
     _1, _2, _3, _4, _5, _6, _7, _8;
+    companion object {
+        fun iterator() = values().asIterable()
+    }
 }
+
+data class ChessCoord(val hor: HorCoord, val vert: VertCoord) {
+    companion object {
+        fun iterator(): Iterable<ChessCoord> {
+            return VertCoord.iterator().reversed().flatMap {
+                vert -> HorCoord.iterator().map { hor -> ChessCoord(hor, vert) }
+            }
+        }
+    }
+
+    fun color() = if ((vert.ordinal + hor.ordinal).isEven()) BLACK else WHITE
+
+    override fun toString() = "$hor$vert"
+}
+
+data class ChessMove(val from: ChessCoord, val to: ChessCoord)
+
+fun Int.isEven() = this % 2 == 0
