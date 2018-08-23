037793b86958774d937c1f4a7327a63b30919d5d
javacook <javacook@gmx.de>
2018-08-23 08:06:08 +0200

    webapps and ChessStuff added

---
+++ src/main/kotlin/de/kotlincook/kotlinchess/ChessStuff.kt
@@ -0,0 +1,16 @@
+package de.kotlincook.kotlinchess
+
+import de.kotlincook.kotlinchess.Color.*
+
+
+enum class Color {
+    BLACK, WHITE
+}
+
+enum class HorCoord {
+    A, B, C, D, E, F, G, H;
+}
+
+enum class VertCoord {
+    _1, _2, _3, _4, _5, _6, _7, _8;
+}
